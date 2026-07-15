38fcf66807522a6653656abdd36de9c8dba48924
Aniket181 <aniketpatil20400@gamil.com>
2026-07-15 11:59:45 +0530

    added area of reactangle feature

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from cal_func import add, subtract
+from cal_multiply import multiply
+from area_of_rectangle import area_of_rectangle
 
 def main():
     print("""Select the function from the given options:
     1. Add
     2. Subtract
     3. multiply
+    4. calculate area of rectangle
           """)
 
     choice = input("Enter your choice (1 or 2): ")
@@ -22,5 +25,12 @@ def main():
         a = float(input("Enter the first number: "))
         b = float(input("Enter the second number: "))
         result = multiply(a, b)
-        print(f"The product of {a} and {b} is: {result}")               
+        print(f"The product of {a} and {b} is: {result}")
+    elif choice == '4':
+        a = float(input("Enter the length of the rectangle: "))         
+        b = float(input("Enter the width of the rectangle: "))
+        result = area_of_rectangle(a, b)
+        print(f"The area of the rectangle with length {a} and width {b} is: {result}") 
+
+                   
         
